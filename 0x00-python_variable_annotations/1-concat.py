37cb3a4848b7f71b2b8d9d6f64a8be7b8c6456ad
Awe Ayomidipupo <pupoawe@gmail.com>
2023-07-08 15:08:44 +0100

    feat: solve task 0

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""This module contains a function
+that adds to floats and returns a float
+"""
+
+
+def add(a: float, b: float) -> float:
+    """Adds a and b and returns the result
+    """
+    return a + b
