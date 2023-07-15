501a15ad41e06ec7afbe141df776bf0cd2125d2c
Awe Ayomidipupo <pupoawe@gmail.com>
2023-07-15 18:25:47 +0100

    feat: solve task 0

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""This module contains code for task
+0 of python async comprehensions
+"""
+
+import random
+import asyncio
+
+
+async def async_generator():
+    """Loops 10 times, each time asynchronously wait 1 second,
+    then yield a random number between 0 and 10.
+    """
+
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.random() * 10
