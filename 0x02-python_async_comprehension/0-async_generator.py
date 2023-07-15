d6f72c25ee4cf2e56b390267b10304fe8665db97
Awe Ayomidipupo <pupoawe@gmail.com>
2023-07-15 18:54:50 +0100

    fix: annotate function

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -7,7 +7,7 @@ import random
 import asyncio
 
 
-async def async_generator():
+async def async_generator() -> float:
     """Loops 10 times, each time asynchronously wait 1 second,
     then yield a random number between 0 and 10.
     """
