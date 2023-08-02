cfb1599d2025a416cd1c1267d8a6c8916b6f045d
Awe Ayomidipupo <pupoawe@gmail.com>
2023-08-02 15:09:17 +0100

    feat: solve task 1

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -22,3 +22,12 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map: Dict, path: Tuple, expected: any) -> None:
         """Tests utils.access_nested_map"""
         self.assertEqual(utils.access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map: Dict, path: Tuple) -> None:
+        """Tests utils.access_nested_map"""
+        with self.assertRaises(KeyError):
+            utils.access_nested_map(nested_map, path)
